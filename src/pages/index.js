e9ea523dcbd293a22b340822a0b887ccdfb583ea
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-05-18 22:58:24 +0500

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,6 @@ const IndexPage = () => {
     <main >
       <title>Home Page</title>
       <h1>Gatsby Home Page</h1>
-      {/* <p>{process.env.CONTENTFUL_ACCESS_TOKEN}</p> */}
     </main>
   )
 }
