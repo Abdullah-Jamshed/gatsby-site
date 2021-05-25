528304f4733aaeef52c9a7ab667240dc4de6d59d
abdullah jamshed <abdullahjamshad01@gmail.com>
2021-05-25 21:23:12 +0500

    richText utilization

--- src/template/productTemplate.js
+++ src/template/productTemplate.js
@@ -1,9 +1,41 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const ProductTemplate = ({data:{contentfulProduct}}) => {
-  console.log(contentfulProduct)
-  return <div>template page</div>
+import { renderRichText } from "gatsby-source-contentful/rich-text"
+import { BLOCKS, MARKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+
+const ProductTemplate = ({ data: { contentfulProduct } }) => {
+  console.log(contentfulProduct.headingsForPage.raw)
+
+  const options = {
+    renderNode: {
+      [BLOCKS.HEADING_1]: (node, childern) => (
+        <h1 style={{ textAlign: "center" }}>{childern}</h1>
+      ),
+    },
+  }
+
+  return (
+    <div>
+      template page
+      <div>
+        <p>{contentfulProduct.headingsForPage.raw}</p>
+
+        <p>Render Rich Text with : gatsby-source-contentful/rich-text</p>
+
+        {contentfulProduct &&
+          renderRichText(contentfulProduct.headingsForPage, options)}
+
+        <p>Render Rich Text with : @contentful/rich-text-react-renderer</p>
+
+        {documentToReactComponents(
+          JSON.parse(contentfulProduct.headingsForPage.raw),
+          options
+        )}
+      </div>
+    </div>
+  )
 }
 
 export const query = graphql`
@@ -12,6 +44,9 @@ export const query = graphql`
       title
       price
       id
+      headingsForPage {
+        raw
+      }
     }
   }
 `
